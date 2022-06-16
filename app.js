f8336a7b5999dd5f5746cc3fd5417e02e8d6ef57
Isadora Ávila <isadorgravila@gmail.com>
2022-06-16 15:17:25 -0300

    bonus get /tweets/username

--- app.js
+++ app.js
@@ -5,8 +5,47 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const usuarios = [];
-const tweets = [];
+const usuarios = [
+    {
+        username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    },
+    {
+        username: "patrick",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    },
+    {
+        username: "molusco",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    }
+];
+const tweets = [
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub 1"
+	},
+    {
+		username: "patrick",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub 2"
+	},
+    {
+		username: "molusco",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub 3"
+	},
+    {
+		username: "molusco",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub 4"
+	},
+    {
+		username: "molusco",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub 5"
+	}
+];
 
 app.post('/sign-up', (req, res) => {
     if (req.body.username === "" || req.body.avatar === "") {
@@ -44,6 +83,23 @@ app.post('/tweets', (req, res) => {
         tweets.push(tweet);
         res.status(201).send('OK');
     }
+});
+
+app.get('/tweets/:username', (req, res) => {
+    const username = req.params.username;
+    const userTweets = tweets.filter(tweet => tweet.username === username);
+    let tweetsPublicados = [];
+    if (userTweets.length <= 10) {
+        for (let i = userTweets.length - 1; i >= 0; i--) {
+            tweetsPublicados.push(userTweets[i]);
+        }
+    } else {
+        for (let i = userTweets.length - 1; i >= userTweets.length - 10; i--) {
+            tweetsPublicados.push(userTweets[i]);
+        }
+    }
+    res.send(tweetsPublicados);
+
 });
 
 app.listen(5000);
